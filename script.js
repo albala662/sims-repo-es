a48470a03da34835e9147798bc7c5158072c5876
albala662 <albala662@gmail.com>
2025-11-14 16:04:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", ()=>{
 ////////////HAMBURGUESA////////////
 const burguer = document.querySelector(".burguir");
 const menu = document.querySelector(".menu");
@@ -7,7 +8,7 @@ if (burguer && menu){
         menu.classList.toggle("active");
     })
 }
-//CARRUSEL
+//////////////CARRUSEL
 const carruselCont = document.querySelector("main>div");
 const images = carruselCont.querySelectorAll("img");
 const btnPrev = carruselCont.querySelector(".prev");
@@ -23,7 +24,7 @@ function mostrarImagen(index){
     })
 }
 
-//PUNTITOS
+/////////////////PUNTITOS
 images.forEach((img, i)=>{
     const punto = document.createElement("span");
     if(i===0) punto.classList.add("active");
@@ -35,7 +36,7 @@ images.forEach((img, i)=>{
 })
 const puntos = puntitosCont.querySelectorAll("span");
 
-//BOTONES 
+///////////////////////////////BOTONES 
 btnNext.addEventListener("click", ()=>{
     indice = (indice+1)%images.length;
     mostrarImagen(indice);
@@ -46,7 +47,7 @@ btnPrev.addEventListener("click", ()=>{
     mostrarImagen(indice);
 })
 
-//PASAR AUTOMÁTICAMENTE LAS IMÁGENES
+///////////////////////////PASAR AUTOMÁTICAMENTE LAS IMÁGENES
 function carruselStart(){
     intervalo= setInterval(()=>{
         indice=(indice+1)%images.length;
@@ -63,3 +64,58 @@ carruselCont.addEventListener("mouseleave", carruselStart);
 
 mostrarImagen(indice);
 carruselStart();
+
+///////////BÚSQUEDA//////////
+const buscador= document.getElementById("buscador");
+const seccion = document.querySelectorAll("main section");
+
+buscador.addEventListener("input", e =>{
+    const texto = e.target.value.toLowerCase();
+    secciones.forEach(sec =>{
+        const titulo = sec.querySelector("h2").textContent.toLowerCase();
+        sec.style.display = titulo.includes(texto) ? "block":"none";
+    })
+})
+////////SCROLL Y VOLVER ARRIBA///////////
+    const arriba = document.getElementById("up");
+
+    window.addEventListener("scroll", ()=>{
+        if(window.scrollY>100){
+            arriba.style.display="flex";
+        } else{
+            arriba.style.display="none";
+        }
+    })
+    arriba.addEventListener("click", ()=>{
+        window.scrollTo({
+            top:0,
+            behavior: "smooth"        
+        })
+    })
+})
+//////////////FORMULARIO///////////
+const nombre= document.getElementById("nombre");
+const correo= document.getElementById("email");
+const mensaje = document.getElementById("mensaje");
+
+nombre.addEventListener("input",()=>{
+    if(nombre.value.length <3){
+        nombre.style.borderColor="red";
+    }else{
+        nombre.style.borderColor = "green";
+    }
+})
+correo.addEventListener("input",()=>{
+    const regex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    correo.style.borderColor = regex.test(correo.value) ? "green":"red";
+})
+
+//////////////ACCESIBILIDAD CON TOGGLE PARA MENÚS EN MÓVILES////////
+document.querySelectorAll(".submenu > a").forEach(link=>{
+    link.addEventListener("click", e =>{
+        if(window.innerWidth <=768){
+            e.preventDefault();
+            link.parentElement.classList.toggle("open");
+        }
+    })
+})    
